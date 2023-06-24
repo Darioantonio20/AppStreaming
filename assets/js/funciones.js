fa3fe4a7a64bd0cc50daa1abf88aadd47641e650
DarioAntonio <antoniodario10@hotmail.com>
2023-06-24 11:02:12 -0600

    Peliculas de traillers subidas

---
+++ assets/js/funciones.js
@@ -0,0 +1,259 @@
+const btn = document.querySelector('.btn');
+const videoContainer = document.querySelector('.video-container');
+const close = document.querySelector('.close');
+
+btn.addEventListener('click', () => {
+    videoContainer.classList.add('show');
+});
+close.addEventListener('click', () => {
+    videoContainer.classList.remove('show');
+});
+
+///////
+
+const btn1 = document.querySelector('.btn1');
+                const videoContainer1 = document.querySelector('.video-container1');
+                const close1 = document.querySelector('.close1');
+                
+                btn1.addEventListener('click', () => {
+                    videoContainer1.classList.add('show');
+                });
+                close1.addEventListener('click', () => {
+                    videoContainer1.classList.remove('show');
+                });
+                
+///////
+
+const btn2 = document.querySelector('.btn2');
+                const videoContainer2 = document.querySelector('.video-container2');
+                const close2 = document.querySelector('.close2');
+                
+                btn2.addEventListener('click', () => {
+                    videoContainer2.classList.add('show');
+                });
+                close2.addEventListener('click', () => {
+                    videoContainer2.classList.remove('show');
+                });
+                
+///////
+
+const btn3 = document.querySelector('.btn3');
+                const videoContainer3 = document.querySelector('.video-container3');
+                const close3 = document.querySelector('.close3');
+                
+                btn3.addEventListener('click', () => {
+                    videoContainer3.classList.add('show');
+                });
+                close3.addEventListener('click', () => {
+                    videoContainer3.classList.remove('show');
+                });
+                
+///////
+
+const btn4 = document.querySelector('.btn4');
+                const videoContainer4 = document.querySelector('.video-container4');
+                const close4 = document.querySelector('.close4');
+                
+                btn4.addEventListener('click', () => {
+                    videoContainer4.classList.add('show');
+                });
+                close4.addEventListener('click', () => {
+                    videoContainer4.classList.remove('show');
+                });
+                
+///////
+
+const btn5 = document.querySelector('.btn5');
+                const videoContainer5 = document.querySelector('.video-container5');
+                const close5 = document.querySelector('.close5');
+                
+                btn5.addEventListener('click', () => {
+                    videoContainer5.classList.add('show');
+                });
+                close5.addEventListener('click', () => {
+                    videoContainer5.classList.remove('show');
+                });
+                
+///////
+
+const btn6 = document.querySelector('.btn6');
+                const videoContainer6 = document.querySelector('.video-container6');
+                const close6 = document.querySelector('.close6');
+                
+                btn6.addEventListener('click', () => {
+                    videoContainer6.classList.add('show');
+                });
+                close6.addEventListener('click', () => {
+                    videoContainer6.classList.remove('show');
+                });
+                
+///////
+
+const btn7 = document.querySelector('.btn7');
+                const videoContainer7 = document.querySelector('.video-container7');
+                const close7 = document.querySelector('.close7');
+                
+                btn7.addEventListener('click', () => {
+                    videoContainer7.classList.add('show');
+                });
+                close7.addEventListener('click', () => {
+                    videoContainer7.classList.remove('show');
+                });
+                
+///////
+
+const btn8 = document.querySelector('.btn8');
+                const videoContainer8 = document.querySelector('.video-container8');
+                const close8 = document.querySelector('.close8');
+                
+                btn8.addEventListener('click', () => {
+                    videoContainer8.classList.add('show');
+                });
+                close8.addEventListener('click', () => {
+                    videoContainer8.classList.remove('show');
+                });
+                
+///////
+
+const btn9 = document.querySelector('.btn9');
+                const videoContainer9 = document.querySelector('.video-container9');
+                const close9 = document.querySelector('.close9');
+                
+                btn9.addEventListener('click', () => {
+                    videoContainer9.classList.add('show');
+                });
+                close9.addEventListener('click', () => {
+                    videoContainer9.classList.remove('show');
+                });
+                
+///////
+
+const btn10 = document.querySelector('.btn10');
+                const videoContainer10 = document.querySelector('.video-container10');
+                const close10 = document.querySelector('.close10');
+                
+                btn10.addEventListener('click', () => {
+                    videoContainer10.classList.add('show');
+                });
+                close10.addEventListener('click', () => {
+                    videoContainer10.classList.remove('show');
+                });
+                
+///////
+
+const btn11 = document.querySelector('.btn11');
+                const videoContainer11 = document.querySelector('.video-container11');
+                const close11 = document.querySelector('.close11');
+                
+                btn11.addEventListener('click', () => {
+                    videoContainer11.classList.add('show');
+                });
+                close11.addEventListener('click', () => {
+                    videoContainer11.classList.remove('show');
+                });
+                
+///////
+
+const btn12 = document.querySelector('.btn12');
+                const videoContainer12 = document.querySelector('.video-container12');
+                const close12 = document.querySelector('.close12');
+                
+                btn12.addEventListener('click', () => {
+                    videoContainer12.classList.add('show');
+                });
+                close12.addEventListener('click', () => {
+                    videoContainer12.classList.remove('show');
+                });
+                
+///////
+
+const btn13 = document.querySelector('.btn13');
+                const videoContainer13 = document.querySelector('.video-container13');
+                const close13 = document.querySelector('.close13');
+                
+                btn13.addEventListener('click', () => {
+                    videoContainer13.classList.add('show');
+                });
+                close13.addEventListener('click', () => {
+                    videoContainer13.classList.remove('show');
+                });
+                
+///////
+
+const btn14 = document.querySelector('.btn14');
+                const videoContainer14 = document.querySelector('.video-container14');
+                const close14 = document.querySelector('.close14');
+                
+                btn14.addEventListener('click', () => {
+                    videoContainer14.classList.add('show');
+                });
+                close14.addEventListener('click', () => {
+                    videoContainer14.classList.remove('show');
+                });
+                
+///////
+
+const btn15 = document.querySelector('.btn15');
+                const videoContainer15 = document.querySelector('.video-container15');
+                const close15 = document.querySelector('.close15');
+                
+                btn15.addEventListener('click', () => {
+                    videoContainer15.classList.add('show');
+                });
+                close15.addEventListener('click', () => {
+                    videoContainer15.classList.remove('show');
+                });
+                
+///////
+
+const btn16 = document.querySelector('.btn16');
+                const videoContainer16 = document.querySelector('.video-container16');
+                const close16 = document.querySelector('.close16');
+                
+                btn16.addEventListener('click', () => {
+                    videoContainer16.classList.add('show');
+                });
+                close16.addEventListener('click', () => {
+                    videoContainer16.classList.remove('show');
+                });
+                
+///////
+
+const btn17 = document.querySelector('.btn17');
+                const videoContainer17 = document.querySelector('.video-container17');
+                const close17 = document.querySelector('.close17');
+                
+                btn17.addEventListener('click', () => {
+                    videoContainer17.classList.add('show');
+                });
+                close17.addEventListener('click', () => {
+                    videoContainer17.classList.remove('show');
+                });
+                
+///////
+
+const btn18 = document.querySelector('.btn18');
+                const videoContainer18 = document.querySelector('.video-container18');
+                const close18 = document.querySelector('.close18');
+                
+                btn18.addEventListener('click', () => {
+                    videoContainer18.classList.add('show');
+                });
+                close18.addEventListener('click', () => {
+                    videoContainer18.classList.remove('show');
+                });
+                
+///////
+
+const btn19 = document.querySelector('.btn19');
+                const videoContainer19 = document.querySelector('.video-container19');
+                const close19 = document.querySelector('.close19');
+                
+                btn19.addEventListener('click', () => {
+                    videoContainer19.classList.add('show');
+                });
+                close19.addEventListener('click', () => {
+                    videoContainer19.classList.remove('show');
+                });
+                
+///////
